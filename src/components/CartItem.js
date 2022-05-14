64905054d133104679285f6c661b9596912a814f
Akshay Benny <akshay@pop-os.localdomain>
2022-05-14 18:09:03 +0530

    cart remove functionality added

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { ChevronDown, ChevronUp } from '../icons';
+import { removeItem } from '../redux/cart/cartSlice';
+import { useDispatch } from 'react-redux';
 const CartItem = ({ id, img, title, price, amount }) => {
+  const dispatch = useDispatch();
   return (
     <article className='cart-item'>
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
         <h4 className='item-price'>${price}</h4>
-        <button className='remove-btn'>remove</button>
+        <button onClick={() => dispatch(removeItem(id))} className='remove-btn'>remove</button>
       </div>
       <div>
         <button className='amount-btn'>
           <ChevronUp />
         </button>
         <p className='amount'>{amount}</p>
-        <button className='amount-btn'>
+        <button className='amount-btn' >
           <ChevronDown />
         </button>
       </div>
--- src/redux/cart/cartSlice.js
+++ src/redux/cart/cartSlice.js
@@ -14,11 +14,16 @@ export const cartSlice = createSlice({
   reducers: {
     clearCart: (state) => {
       state.cartItems = [];
+      //if return is used in this reducer instead of state.cartItems = [], then it will return a new initial state, i.e., if we return {} then initialState or the new state will be set to {}
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter(item=>item.id !== itemId)
     },
   },
 });
 
 // console.log(cartSlice);
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
