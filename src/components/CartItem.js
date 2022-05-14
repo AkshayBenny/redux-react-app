f68517804a9b541beda8f17014097c324367293e
Akshay Benny <akshay@pop-os.localdomain>
2022-05-14 18:20:35 +0530

    change quantity functions added

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ChevronDown, ChevronUp } from '../icons';
-import { removeItem } from '../redux/cart/cartSlice';
+import { removeItem, increase, decrease } from '../redux/cart/cartSlice';
 import { useDispatch } from 'react-redux';
 const CartItem = ({ id, img, title, price, amount }) => {
   const dispatch = useDispatch();
@@ -10,14 +10,16 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <div>
         <h4>{title}</h4>
         <h4 className='item-price'>${price}</h4>
-        <button onClick={() => dispatch(removeItem(id))} className='remove-btn'>remove</button>
+        <button onClick={() => dispatch(removeItem(id))} className='remove-btn'>
+          remove
+        </button>
       </div>
       <div>
-        <button className='amount-btn'>
+        <button onClick={() => dispatch(increase(id))} className='amount-btn'>
           <ChevronUp />
         </button>
         <p className='amount'>{amount}</p>
-        <button className='amount-btn' >
+        <button onClick={() => dispatch(decrease(id))} className='amount-btn'>
           <ChevronDown />
         </button>
       </div>
